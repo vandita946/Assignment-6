f17062f487ee24321779072d9437bbeffca76200
swapnil9595 <65943650+swapnil9595@users.noreply.github.com>
2021-04-19 14:08:07 -0400

    handin errors fixed

--- test/ViewTest.java
+++ test/ViewTest.java
@@ -34,35 +34,36 @@ public class ViewTest {
   public void testSVGGetDescription() {
     View svg = new SVGView(model, "System.out");
     assertEquals("<svg width=\"0\" height=\"0\" viewBox=\"0 0 0 0\" version=\"1.1\"\n"
-        + "\txmlns=\"http://www.w3.org/2000/svg\">\n"
-        + "<ellipse id=\"circly\" cx=\"0\" cy=\"5\" rx=\"10\" ry=\"10\" fill=\""
-        + "rgb(255,255,255)\" visibility=\"visible\" >\n"
-        + "\t<animate attributeType=\"xml\" begin=\"10000ms\" dur=\"40000ms\" "
-        + "attributeName=\"cx\" from=\"0.0\" to=\"10.0\" fill=\"freeze\" />\n"
-        + "\t<animate attributeType=\"xml\" begin=\"10000ms\" dur=\"40000ms\" "
-        + "attributeName=\"cy\" from=\"5.0\" to=\"20.0\" fill=\"freeze\" />\n"
-        + "\t<animate attributeType=\"xml\" begin=\"10000ms\" dur=\"40000ms\" "
-        + "attributeName=\"fill\" from=\"rgb(255,255,255)\" to=\"rgb(0,255,255)\" fill=\"freeze\" />\n"
-        + "\t<animate attributeType=\"xml\" begin=\"10000ms\" dur=\"40000ms\" "
-        + "attributeName=\"cx\" from=\"10.0\" to=\"20.0\" fill=\"freeze\" />\n"
-        + "\t<animate attributeType=\"xml\" begin=\"10000ms\" dur=\"40000ms\" "
-        + "attributeName=\"cy\" from=\"10.0\" to=\"40.0\" fill=\"freeze\" />\n"
-        + "</ellipse>\n"
-        + "</svg>", svg.getDescription());
+                 + "\txmlns=\"http://www.w3.org/2000/svg\">\n"
+                 + "<ellipse id=\"circly\" cx=\"0\" cy=\"5\" rx=\"10\" ry=\"10\" fill=\""
+                 + "rgb(255,255,255)\" visibility=\"visible\" >\n"
+                 + "\t<animate attributeType=\"xml\" begin=\"10000ms\" dur=\"40000ms\" "
+                 + "attributeName=\"cx\" from=\"0.0\" to=\"10.0\" fill=\"freeze\" />\n"
+                 + "\t<animate attributeType=\"xml\" begin=\"10000ms\" dur=\"40000ms\" "
+                 + "attributeName=\"cy\" from=\"5.0\" to=\"20.0\" fill=\"freeze\" />\n"
+                 + "\t<animate attributeType=\"xml\" begin=\"10000ms\" dur=\"40000ms\" "
+                 + "attributeName=\"fill\" from=\"rgb(255,255,255)\" to=\"rgb(0,255,255)\" "
+                 + "fill=\"freeze\" />\n"
+                 + "\t<animate attributeType=\"xml\" begin=\"10000ms\" dur=\"40000ms\" "
+                 + "attributeName=\"cx\" from=\"10.0\" to=\"20.0\" fill=\"freeze\" />\n"
+                 + "\t<animate attributeType=\"xml\" begin=\"10000ms\" dur=\"40000ms\" "
+                 + "attributeName=\"cy\" from=\"10.0\" to=\"40.0\" fill=\"freeze\" />\n"
+                 + "</ellipse>\n"
+                 + "</svg>", svg.getDescription());
   }
 
   @Test
   public void testTextGetDescription() {
     View textual = new TextualView(model, "System.out");
     assertEquals("Create RGB(255,255,255) oval circly with center at (0,5), radius 10 and 10\n"
-            + "\n"
-            + "circly appears at time t=10000.0 and disappears at time t=50000.0\n"
-            + "\n"
-            + "circly moves from (0,5) to (10,20) from time t=10000 to t=50000\n"
-            + "circly changes from RGB(255,255,255) to RGB(0,255,255) from time t=10000 to "
-            + "t=50000\n"
-            + "circly changes radius from 10 to 20 and other radius from 10 to 40 from time "
-            + "t=10000 to t=50000\n",
+                 + "\n"
+                 + "circly appears at time t=10000.0 and disappears at time t=50000.0\n"
+                 + "\n"
+                 + "circly moves from (0,5) to (10,20) from time t=10000 to t=50000\n"
+                 + "circly changes from RGB(255,255,255) to RGB(0,255,255) from time t=10000 to "
+                 + "t=50000\n"
+                 + "circly changes radius from 10 to 20 and other radius from 10 to 40 from time "
+                 + "t=10000 to t=50000\n",
         textual.getDescription());
   }
 
